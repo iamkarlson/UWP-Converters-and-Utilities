98ace33e8aa5099db57a466c95bf1193394cd552
Tommaso Scalici <tommaso.scalici@outlook.it>
2016-07-14 18:04:27 +0200

    Added DispatcherExtensions.

---
+++ UWPConvertersAndUtilities/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("UWP Converters and Utilities")]
+[assembly: AssemblyDescription("A collection of useful converters and utilities for UWA development.")]
+[assembly: AssemblyCompany("Tommaso Scalici")]
+[assembly: AssemblyProduct("UWP Converters and Utilities")]
+[assembly: AssemblyCopyright("Copyright © 2016 Tommaso Scalici")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
